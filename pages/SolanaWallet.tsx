cdfa9c9834a8e84173030babe2b350c6932a6063
Albert <67883068+quan1354@users.noreply.github.com>
2024-01-26 22:35:53 +0800

    Your commit message

--- pages/SolanaWallet.tsx
+++ pages/SolanaWallet.tsx
@@ -5,7 +5,6 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter,
   SkyWalletAdapter, 
   AlphaWalletAdapter, 
@@ -19,20 +18,14 @@ import { PhantomWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 import {
   WalletModalProvider,
-  WalletDisconnectButton,
   WalletMultiButton,
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-
+import * as web3 from "@solana/web3.js"
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 export const SolanaWallet: FC = () => {
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const endpoint = web3.clusterApiUrl("devnet")
 
   const wallets = useMemo(
     () => [
@@ -48,7 +41,7 @@ export const SolanaWallet: FC = () => {
       new KeystoneWalletAdapter(),
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [network],
+    [],
   );
 
   return (
@@ -66,3 +59,5 @@ export const SolanaWallet: FC = () => {
     </ConnectionProvider>
   );
 };
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-import { FC, FormEventHandler, useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable prettier/prettier */
+import { FC, useEffect, useState} from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import {
@@ -23,12 +26,84 @@ import { standardFetcher } from 'utils';
 import { FAQItem, getFaqList } from 'utils/faqList';
 import { SolanaWallet } from './SolanaWallet';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import idl from './idl.json';
+import {
+  Wallet,
+  useAnchorWallet,
+  useConnection,
+} from '@solana/wallet-adapter-react';
+import {
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionSignature,
+} from '@solana/web3.js';
+import {
+  MessageSignerWalletAdapterProps,
+  SendTransactionOptions,
+  SignerWalletAdapterProps,
+  WalletName,
+} from '@solana/wallet-adapter-base';
+// import {
+//   AnchorProvider,
+//   Idl,
+//   Program,
+//   setProvider,
+// } from '@project-serum/anchor';
+import * as anchor from "@project-serum/anchor"
 interface HomeProps {
+  setTransactionUrl: any;
   faqList: FAQItem[];
+  // Wallet Context State
+  autoConnect: boolean;
+  wallets: Wallet[];
+  wallet: Wallet | null;
+  publicKey: PublicKey | null;
+  connecting: boolean;
+  connected: boolean;
+  disconnecting: boolean;
+  select(walletName: WalletName): void;
+  connect(): Promise<void>;
+  disconnect(): Promise<void>;
+  sendTransaction(
+    transaction: Transaction,
+    connection: Connection,
+    options?: SendTransactionOptions,
+  ): Promise<TransactionSignature>;
+  signTransaction: SignerWalletAdapterProps['signTransaction'] | undefined;
+  signAllTransactions:
+    | SignerWalletAdapterProps['signAllTransactions']
+    | undefined;
+  signMessage: MessageSignerWalletAdapterProps['signMessage'] | undefined;
+  
 }
 
-const Home: FC<HomeProps> = ({ faqList }) => {
+
+
+const Home: FC<HomeProps> = ({ faqList, setTransactionUrl }) => {
+  const [program, setProgram] = useState<anchor.Program>()
+  const { connection } = useConnection();
+  const wallet = useAnchorWallet();
+
+  // const provider = new AnchorProvider(connection, wallet!, {});
+  // setProvider(provider);
+
+  const PROGRAM_ID = new anchor.web3.PublicKey(`CdKFiRMHeUX6P74CfXRwnidMPqDgtbbCsN59iH8Kudwq`)
+  //const programId = new PublicKey('CdKFiRMHeUX6P74CfXRwnidMPqDgtbbCsN59iH8Kudwq');
+  //const program = new Program(idl as Idl, programId);
   useEffect(() => {
+    let provider: anchor.Provider
+    try {
+      provider = anchor.getProvider()
+    } catch {
+      provider = new anchor.AnchorProvider(connection, wallet!, {})
+      anchor.setProvider(provider)
+    }
+
+    const program = new anchor.Program(idl as anchor.Idl, PROGRAM_ID)
+    setProgram(program)
+
     const matomoSomeEvent: MatomoEventType = [
       'Lido_Frontend_Template',
       'Mount index component',
@@ -38,18 +113,28 @@ const Home: FC<HomeProps> = ({ faqList }) => {
     trackEvent(...matomoSomeEvent);
   }, []);
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> | undefined = (
-    event,
-  ) => {
-    event.preventDefault();
-    alert('Submitted');
-  };
-
   const contractRpc = useSTETHContractRPC();
   const tokenName = useContractSWR({
     contract: contractRpc,
     method: 'name',
   });
+
+  const withdrawToken = async () => {
+    const newAccount = anchor.web3.Keypair.generate()
+    const accounts = await program?.account.counter.all()
+    console.log(accounts)
+
+    const sig = await program?.methods
+      .initialize()
+      .accounts({
+        counter: newAccount.publicKey,
+      })
+      .signers([newAccount])
+      .rpc()
+
+    setTransactionUrl(`https://explorer.solana.com/tx/${sig}?cluster=devnet`)
+  };
+
 
   // UI -> migrate to new file -> Navigation
   const { data } = useLidoSWR<number>('/api/oneinch-rate', standardFetcher);
@@ -58,10 +143,10 @@ const Home: FC<HomeProps> = ({ faqList }) => {
   return (
     <Layout title="Withdrawals" subtitle="Withdraw unstaked SOL">
       <Head>
-        <title>Lido | Frontend Template</title>
+        <title>EigenSol</title>
       </Head>
       <Block>
-        <form action="" method="post" onSubmit={handleSubmit}>
+        <form action="" method="post">
           {/* <InputWrapper>
             <Input
               fullwidth
@@ -84,7 +169,12 @@ const Home: FC<HomeProps> = ({ faqList }) => {
 
           <SolanaWallet></SolanaWallet>
 
-          <Button style={{ marginTop: '8px' }} fullwidth type="submit">
+          <Button
+            onClick={withdrawToken}
+            style={{ marginTop: '8px' }}
+            fullwidth
+            type="submit"
+          >
             Withdraw
           </Button>
 
