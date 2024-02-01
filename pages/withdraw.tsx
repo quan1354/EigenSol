1000b45737b48720a2930ba36a8ff5a86ea6fe58
1HoWK <howaikit01@gmail.com>
2024-02-01 17:10:58 +0800

    feat(component): merge withdraw component to main branch

--- pages/withdraw.tsx
+++ pages/withdraw.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { FC } from 'react';
 import Head from 'next/head';
 import { ServicePage } from '@lidofinance/lido-ui';
@@ -5,7 +6,7 @@ import { ServicePage } from '@lidofinance/lido-ui';
 const About: FC = () => (
   <ServicePage title="404">
     <Head>
-      <title>Lido | About</title>
+      <title>EigenSol</title>
     </Head>
   </ServicePage>
 );
